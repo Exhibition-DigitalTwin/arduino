0adc906c999b07533aecadee0b6ebce2e9b13f23
Jan Schneider <jan.schneider@hfg-gmuend.de>
2018-12-18 19:13:27 +0100

    both stepper motors running when turning cabin angle

--- stepper_Motors/stepper_Motors.ino
+++ stepper_Motors/stepper_Motors.ino
@@ -1,42 +1,43 @@
 #include <Stepper.h>
 
-int incomingByte = 0; 
+int incomingByte = 0;
+int previousStepsCabin = 0;
 
-
-const int stepsPerRevolution = 500;  // change this to fit the number of steps per revolution
-// for your motor
+const int stepperRotorBladesSteps = 500;  // change this to fit the number of steps per revolution for your motor
+const int stepperCabinSteps = 500;
 
 // initialize the stepper library on pins 8 through 11:
-Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11);
-Stepper myStepper1(stepsPerRevolution, 4, 6, 5, 7);
+Stepper stepperCabin(stepperCabinSteps, 8, 10, 9, 11);
+Stepper stepperRotorBlades(stepperRotorBladesSteps, 4, 6, 5, 7);
 
 void setup() {
   // set the speed at 60 rpm:
-  myStepper.setSpeed(15);
-  myStepper1.setSpeed(15);
+  stepperCabin.setSpeed(15);
+  stepperRotorBlades.setSpeed(15);
   // initialize the serial port:
   Serial.begin(9600);
 }
 
 void loop() {
-
   if (Serial.available() > 0) {
-                // read the incoming byte:
-                incomingByte = Serial.read();
+    // read the incoming byte:
+    incomingByte = Serial.read();
 
-                // say what you got:
-                Serial.print("I received: ");
-                Serial.println(incomingByte, DEC);
-        }
-        
-  if(incomingByte == 49){
-  Serial.println("clockwise");
-  myStepper.step(stepsPerRevolution);
-  delay(500);
+    // say what you got:
+    Serial.print("I received: ");
+    Serial.println(incomingByte, DEC);
   }
-
-  else {}
-
-  myStepper1.step(stepsPerRevolution);
+  if (incomingByte == 49) {
+    Serial.println("turning cabin angle");
+    while (previousStepsCabin < stepperCabinSteps) {
+      int doSteps = stepperCabinSteps - previousStepsCabin;
+      previousStepsCabin++;
+      stepperCabin.step(1);
+      stepperRotorBlades.step(1);
+    }
+    previousStepsCabin = 0;
+    delay(10);
+  }
+  stepperRotorBlades.step(stepperRotorBladesSteps);
   delay(10);
 }
